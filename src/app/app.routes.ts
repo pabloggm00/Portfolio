284b3ac6e4e4a5306fe0cf56efbb59955eb738e8
Pablo Gonzalez <pabloggm00@gmail.com>
2025-09-02 21:25:02 +0200

    open graph ajustado y 404 page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { ExtraOptions, Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home/home-component';
 import { AboutMeComponent } from './features/aboutme/aboutme/aboutme';
+import { NotFoundComponent } from './features/notfound/notfound';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
-    { path: 'aboutme', component: AboutMeComponent}
+    { path: 'aboutme', component: AboutMeComponent},
+    { path: '**', component: NotFoundComponent }
 ];
 
 
