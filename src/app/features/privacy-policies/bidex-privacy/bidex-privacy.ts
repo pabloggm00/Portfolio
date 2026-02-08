2b9dbe39961008c49616366c1ae95c537aee342f
Pablo Gonzalez <pabloggm00@gmail.com>
2026-02-08 17:33:12 +0100

    bidex privacy

---
+++ src/app/features/privacy-policies/bidex-privacy/bidex-privacy.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bidex-privacy',
+  imports: [],
+  templateUrl: './bidex-privacy.html',
+  styleUrl: './bidex-privacy.scss'
+})
+export class BidexPrivacy {
+  
+}
